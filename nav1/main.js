204d7515473966d2091b13bc01222b87731618a2
webxazim <webxazim@gmail.com>
2024-10-16 22:08:12 +0300

    'init'

---
+++ nav1/main.js
@@ -0,0 +1,8 @@
+function nav(){
+  var middleL = document.getElementById("bottomL")
+  if (middleL.style.display = "block"){
+    middleL.style.display = "none"
+  }else{
+    middleL.style.display = "block"
+  }
+}
